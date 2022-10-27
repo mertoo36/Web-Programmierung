963fc07411b1de7f0a726ef2ac42c94b664d0fc7
Mert <wolf.mert@web.de>
2022-10-27 11:28:53 +0200

    Struktur index Seite

--- app.js
+++ app.js
@@ -12,3 +12,47 @@ var slideIndex = 0;
         slides[slideIndex-1].style.display = "block"; 
         setTimeout(showSlides, 4000); // Change image every 2 seconds
       }
+      var slideIndex = 0;
+      showSlides2();
+      
+      function showSlides2() {
+        var i;
+        var slides = document.getElementsByClassName("mySlides2");
+        for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none"; 
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {slideIndex = 1} 
+        slides[slideIndex-1].style.display = "block"; 
+        setTimeout(showSlides, 4000); // Change image every 2 seconds
+      }
+      var slideIndex = 0;
+      showSlides3();
+      
+      function showSlides3() {
+        var i;
+        var slides = document.getElementsByClassName("mySlides3");
+        for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none"; 
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {slideIndex = 1} 
+        slides[slideIndex-1].style.display = "block"; 
+        setTimeout(showSlides, 4000); // Change image every 2 seconds
+      }
+
+      var slideIndex = 0;
+      showSlides4();
+      
+      function showSlides4() {
+        var i;
+        var slides = document.getElementsByClassName("mySlides4");
+        for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none"; 
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {slideIndex = 1} 
+        slides[slideIndex-1].style.display = "block"; 
+        setTimeout(showSlides, 4000); // Change image every 2 seconds
+      }
+
